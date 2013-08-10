0556a968a7f7dc747737f87ed913c2a05f84f0e1
Bjørnar S. Luteberget <luteberget@gmail.com>
2013-08-10 23:19:12 +0200

    updated example-ofxONI2 to match ofxReprojection updates

--- example-ofxONI2/src/testApp.cpp
+++ example-ofxONI2/src/testApp.cpp
@@ -1,4 +1,7 @@
 #include "testApp.h"
+#include "ofxHighlightRects.h"
+
+ofxHighlightRects highlighter;
 
 void testApp::setup(){
 	ofSetLogLevel(OF_LOG_VERBOSE);
@@ -6,7 +9,19 @@ void testApp::setup(){
 
 	depthcam.init();
 	depthcam.open();
+
+
 	reprojection.init(&depthcam);
+
+	// The projector has a resolution of 1024x768 and is configured
+	// as the right monitor. This command adds space in the ofAppWindow
+	// for the projector. The window can be moved so that the position
+	// matches the projector position on the desktop.
+	reprojection.setupProjector(1024,768,OFX_DIRECTION_RIGHT);
+
+	highlighter.init();
+	highlighter.highlightRect("Projector area", reprojection.getProjectorRectangle());
+	highlighter.highlightRect("Main window", reprojection.getMainWindowRectangle());
 
 }
 
@@ -16,7 +31,23 @@ void testApp::update(){
 }
 
 void testApp::draw(){
-	reprojection.draw();
+	// Simplest possible demo:
+	// reprojection.draw();
+	//
+	
+	// More configurable:
+	
+	if(reprojection.isCalibrating()) {
+		reprojection.drawCalibration();
+	} else {
+		reprojection.drawRenderer();
+	}
+	
+	
+	
+	
+
+
 }
 
 
