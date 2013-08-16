3245ef89af23cef8a26fd6ea28087325b8db2a25
Bjørnar S. Luteberget <luteberget@gmail.com>
2013-08-16 17:24:06 +0200

    Simplified example-ofxONI2 to simplest possible. Other examples can show more funcionality.

--- example-ofxONI2/src/testApp.cpp
+++ example-ofxONI2/src/testApp.cpp
@@ -16,7 +16,7 @@ void testApp::setup(){
 	reprojection.setupProjector(1024,768,OFX_DIRECTION_RIGHT);
 
 	// Load calibration data to skip the calibration stage.
-	reprojection.loadCalibrationData("2013-06-30-20-00-02-291.xml");
+	//reprojection.loadCalibrationData("2013-06-30-20-00-02-291.xml");
 	//reprojection.finalizeCalibration();
 	
 }
@@ -28,30 +28,7 @@ void testApp::update(){
 
 void testApp::draw(){
 	// Simplest possible demo:
-	// reprojection.draw();
-	//
-	
-	// More configurable:
-	
-	if(reprojection.isCalibrating()) {
-		reprojection.drawCalibration();
-	} else {
-		depthcam.draw(ofRectangle(10,40,640/4,480/4) + reprojection.getMainWindowPoint());
-
-		ofPushStyle();
-		ofSetColor(ofColor::black);
-		string str = "framerate is ";
-		str += ofToString(ofGetFrameRate(), 2)+"fps";
-		ofDrawBitmapString(str, 20,20);
-		ofPopStyle();
-
-		// Draw a default demo image (hue depth image) transformed to reproject
-		// onto the world. Will be drawn at the projector location defined by 
-		// setupProjector(). Also, reprojection.renderer.setDrawArea can be used
-		// to specify rendering area manually.
-		reprojection.drawRenderer();
-	}
-
+	reprojection.draw();
 }
 
 void testApp::keyPressed(int key){
